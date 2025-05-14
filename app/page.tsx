dd78ed981bd864260e6e3be51b1099ed034d645d
papiniyasmin <yasminpapiniii@gmail.com>
2025-05-14 15:00:46 +0100

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -5,10 +5,12 @@ import TecnologiaConsultoria from '../components/TecnologiaConsultoria';
 import DentroVisualforma from '../components/DentroVisualforma';
 import Clientes from '../components/Clientes';
 import VisualformaNumeros from '../components/VisualformaNumeros';
+import Index from '../components/Index';
 
 export default function Home() {
   return (
     <main>
+      < Index />
       <TecnologiaConsultoria />
       <VisualformaNumeros />
       <Clientes />
